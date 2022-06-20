37ebaeacd9daf4d37949b496e130353496c1d1f2
saiteja0804 <99531351+saiteja0804@users.noreply.github.com>
2022-06-20 17:05:47 +0530

    Add files via upload

---
+++ python programming/clas_obj.py
@@ -0,0 +1,10 @@
+class hello:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+        print("hai")
+    def python(self):
+        print(self.a,self.b) 
+
+hello(10,20)
+hello.python(30,40)
